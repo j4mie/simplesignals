ab96392152eed349faaa7e1a3c5d11f524e7a8c3
Jamie Matthews <jamie.matthews@gmail.com>
2012-07-08 00:33:07 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simplesignals',
-    version='0.1.0',
+    version='0.2.0',
     description='Unix signal handlers and worker processes, simplified',
     author='Jamie Matthews',
     author_email='jamie.matthews@gmail.com',
